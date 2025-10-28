b5bd8a8b4636e28556a8e0b96d9c35f5069e5193
Akmal192003 <akmalsyazwan19@gmail.com>
2025-10-28 14:11:40 +0800

    add new index.php for admin

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<?php
+include("connection.php");
+include("header.php");
+
+if(!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
+    echo "<script> alert('Access denied.'); </script>";
+    header("Location: ./admin/index.php");
+    exit();
+}
+
+if(!isset($_SESSION["user_id"])) {
+    echo "<script> alert('Please Login'); </script>";
+    header("Location: ./admin/index.php");
+    exit(); // Always exit after a header redirect
+}
+
+?>
+
+<body>
+    <main>
+        <div>
+            <p>this is admin dashboard that can go to manage user or else gbye</p>
+        </div>
+    </main>
+</body>
