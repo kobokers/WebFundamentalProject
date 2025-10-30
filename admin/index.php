bd94699ceab749a2696a1bfd52f6d8a7e9f1643a
Akmal192003 <akmalsyazwan19@gmail.com>
2025-10-31 07:57:50 +0800

    commented bugged code (going to fix later)

--- admin/index.php
+++ admin/index.php
@@ -2,17 +2,17 @@
 include("connection.php");
 include("header.php");
 
-if(!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
-    echo "<script> alert('Access denied.'); </script>";
-    header("Location: ./admin/index.php");
-    exit();
-}
+// if(!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
+//     echo "<script> alert('Access denied.'); </script>";
+//     header("Location: ./index.php");
+//     exit();
+// }
 
-if(!isset($_SESSION["user_id"])) {
-    echo "<script> alert('Please Login'); </script>";
-    header("Location: ./admin/index.php");
-    exit(); // Always exit after a header redirect
-}
+// if(!isset($_SESSION["user_id"])) {
+//     echo "<script> alert('Please Login'); </script>";
+//     header("Location: ../index.php");
+//     exit(); // Always exit after a header redirect
+// }
 
 ?>
 
--- index.php
+++ index.php
@@ -4,17 +4,17 @@ $pageTitle = "Home - OLMS";
 include('connection.php');
 include('header.php');
 
-if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
-    echo "<script> alert('Access denied.'); </script>";
-    header("Location: ./admin/index.php");
-    exit();
-}
+// if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
+//     echo "<script> alert('Access denied.'); </script>";
+//     header("Location: ./index.php");
+//     exit();
+// }
 
-if (!isset($_SESSION["user_id"])) {
-    echo "<script> alert('Please Login'); </script>";
-    header("Location: ./admin/index.php");
-    exit(); // Always exit after a header redirect
-}
+// if (!isset($_SESSION["user_id"])) {
+//     echo "<script> alert('Please Login'); </script>";
+//     header("Location: ./index.php");
+//     exit(); // Always exit after a header redirect
+// }
 ?>
 
 <body>
