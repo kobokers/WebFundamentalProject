04c7f8f0d485ca3aab49df5d25e7703ca4497ac7
Akmal192003 <akmalsyazwan19@gmail.com>
2025-11-03 13:40:49 +0800

    add message for the login page if successfully register

--- auth/login.php
+++ auth/login.php
@@ -1,10 +1,20 @@
 <?php
+session_start();
 include("../connection.php");
 include("../header.php");
 ?>
 
 <body>
     <div>
+        <!-- will add flash animation later -->
+        <?php if (isset($_SESSION['error'])): ?>
+            <div class="p-3 mb-4 bg-red-100 border border-red-400 text-red-700 rounded"><?php echo $_SESSION['error'];
+            unset($_SESSION['error']); ?></div>
+        <?php endif; ?>
+        <?php if (isset($_SESSION['success'])): ?>
+            <div class="p-3 mb-4 bg-green-100 border border-green-400 text-green-700 rounded"><?php echo $_SESSION['success'];
+            unset($_SESSION['success']); ?></div>
+        <?php endif; ?>
         <h2 class="text-3xl font-bold text-center mt-10">Login to Your Account</h2>
         <form action="authenticate.php" method="POST" class="max-w-md mx-auto mt-6 bg-white p-6 rounded shadow-md">
             <div class="mb-4">
